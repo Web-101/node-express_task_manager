dd8824b249661fc42c4737e3550cc77b3be8c2e5
BearingMe <gomes.bruno201@gmail.com>
2022-01-07 19:19:30 -0300

    feat: inicializa o express

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,10 @@
-console.log("Hello World");
+import express from 'express';
+
+const app: express.Application = express();
+const port: any = process.env.PORT || 3000;
+
+app.get('/', (req: express.Request, res: express.Response) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => console.log('Listening on port', port));
