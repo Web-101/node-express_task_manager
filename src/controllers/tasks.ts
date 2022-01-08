b0442193d50db38fe1826b3da5fd233e6e1a264d
BearingMe <gomes.bruno201@gmail.com>
2022-01-08 17:58:12 -0300

    feat: adiciona funcionalidade para a rota de criação de task

--- src/controllers/tasks.ts
+++ src/controllers/tasks.ts
@@ -1,12 +1,19 @@
+// import libs
 import express from "express";
+
+// import files
+import taskModel from "../models/tasks";
 
 // no query params
 export function getAllTasks(req: express.Request, res: express.Response) {
   res.send("all tasks here!");
 }
 
-export function createTask(req: express.Request, res: express.Response) {  
-  res.send("create task here!");
+export function createTask(req: express.Request, res: express.Response) {
+  taskModel
+    .create(req.body)
+    .then((task) => res.status(201).send(task))
+    .catch((err) => res.status(400).send(err));
 }
 
 // query by id
