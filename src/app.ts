8664459493ff1f2ed4c266aeb2e02989bba1a71e
BearingMe <gomes.bruno201@gmail.com>
2022-01-10 12:21:19 -0300

    feat: adiciona views
    adiciona toda a views do projeto: index, edit, new, task (equivalente
    ao show ou read)

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,17 @@
 // imports
 import "dotenv/config";
 import express from "express";
+import path from "path";
 import * as db from "./db/connect";
 import tasks from "./routes/tasksRouter";
 
 // constants
 const app: express.Application = express();
 const port: any = process.env.PORT || 3000;
+const publicPath: string = path.join(__dirname, "./public");
 
 // load middlewares
+app.use(express.static(publicPath));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
